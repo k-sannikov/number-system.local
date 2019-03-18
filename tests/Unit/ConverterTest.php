4f468409955754f8f9b4e18bdb0214955062f6a8
Kirill Sannikov <kir.sannikow@mail.ru>
2019-03-18 14:38:19 +0300

    Created Unit-test for Converter

---
+++ tests/Unit/ConverterTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use Converter;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ConverterTest extends TestCase
+{
+    /**
+     * Тестирование метода convert2to10
+     *
+     * @param string $binaryNumber двоичное число
+     * @param integer|double $expected ожидаемый результат
+     *
+     * @return void
+     *
+     * @dataProvider convert2to10Provider
+     */
+    public function testConvert2to10($binaryNumber, $expected)
+    {
+        $this->assertEquals(7, Converter::convert2to10('111'));
+    }
+
+    public function convert2to10Provider()
+    {
+        return [
+            'положительное' => ['111', 7],
+            'отрицательное' => ['-111', -7],
+            'ноль' => ['0', 0],
+            'дробное' => ['111,11', -7,75],
+        ];
+    }
+}
