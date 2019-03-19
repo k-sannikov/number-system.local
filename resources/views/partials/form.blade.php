e9a014b474183ba4947c8fb5aafd98b189243f83
Kirill Sannikov <kir.sannikow@mail.ru>
2019-03-19 18:11:34 +0300

    Corrected form.blade.php

--- resources/views/partials/form.blade.php
+++ resources/views/partials/form.blade.php
@@ -1,7 +1,7 @@
 <div class="input-group mb-3">
     <input type="text" class="form-control" name="binaryNumber" value="{{ $binaryNumber ?? old('binaryNumber', '') }}" placeholder="Двоичное число">
     <div class="input-group-append">
-        <button type="submit" name="operation" value="convert" id="convert" class="btn btn-outline-secondary" >Преобразовать
+        <button type="submit" name="convert2to10" value="convert" id="convert" class="btn btn-outline-secondary" >Преобразовать
         </button>
     </div>
 </div>
